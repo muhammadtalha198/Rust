6c6f1264dad33377c1435a21b5dd82df5feee123
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2022-08-05 10:53:20 +0500

    Create Constants.rs

---
+++ Rust/Constants.rs
@@ -0,0 +1,12 @@
+//Constant in rust
+
+fn main() {
+    // const APVALUE = 12; //error
+    // must define datatype with any constant
+    //must be Capitalize every character of its name during declaration
+
+    const FATHERCNIC: i128 = 1234567891011;
+    println!("{}", FATHERCNIC);
+
+
+}
