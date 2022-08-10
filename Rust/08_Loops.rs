fd078f2460f3830c6745cda94049b7f163e25462
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2022-08-10 21:12:57 +0500

    Create 08_Loops.rs

---
+++ Rust/08_Loops.rs
@@ -0,0 +1,17 @@
+// loops in Rust
+
+fn main() {
+    //while loop in rust
+
+    let mut a = 1;
+    while a <= 10 {
+        println!("The value of a is {}", a);
+        a += 1;
+    }
+
+    // for loop in rust
+
+    for x in 1..=10 {
+        println!("The value of x is {}", x);
+    }
+}
