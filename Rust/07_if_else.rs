d71fb0386da092e64d5a56b9cde978d9f7c3c235
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2022-08-10 21:06:58 +0500

    Update 07_if_else.rs

--- Rust/07_if_else.rs
+++ Rust/07_if_else.rs
@@ -1,29 +1,36 @@
 // if else in Rust
 
 fn main() {
-
     // Example 1
 
-    let a = 12;
-    let b = 18;
+    let a: i16 = 12;
+    let b: i16 = 18;
 
     if a == b {
         println!("a == b");
     } else {
         println!("a != b")
     }
-    
+
     // Example 2
-    
-    let peice = 12;
-    
+
+    let price: i8 = 12;
+
     if price == 1 {
         println!("price is 1.");
-    }
-    else if price == 2 {
+    } else if price == 2 {
         println!("price is 2.");
-    }
-    else{
+    } else {
         println!("price is 12.");
     }
+
+    // Example 3
+
+    let formal: bool = true;
+
+    let greeting: () = if formal {
+        println!("Good Evening!");
+    } else {
+        println!("Good Night!");
+    };
 }
