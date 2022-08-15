0a32e1fe2c75586009e8d8605e99f14e20a8ea80
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2022-08-15 22:20:34 +0500

    Update TodaysWork.rs

--- Rust/TodaysWork.rs
+++ Rust/TodaysWork.rs
@@ -1,70 +1,28 @@
-use reqwest;
 
-// tokio let's us use "async" on our main function
-// #[tokio::main]
-// async fn main() {
-//     // chaining .await will yield our query result
-//     let result = reqwest::get("http://127.0.0.1:5500/talha.html").await
+use std::collections::HashMap;
+use dotenv :: dotenv;
+// use std :: env;
 
-//     println!("{:?}", result)
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    dotenv().ok();
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
+}
+
+// async fn short_way_to_create_http_request() {
+
+//     let body = reqwest::get("https://www.rust-lang.org")
+//     .await?
+//     .text()
+//     .await?;
+
+//     println!("body = {:?}", body);
+//     return;
 // }
 
-fn main() {
-    short_way_to_create_http_request();
-}
-
-async fn short_way_to_create_http_request() {
-
-     let result = reqwest::get("https://api.spotify.com/v1/search").await
-
-    println!("{:?}", result);
-
-}
-
-//////////////Method 2//////////////////////////////////////////////////////
-
-// how to make http Get request
-
-extern crate reqwest;
-
-fn main() {
-    short_way_to_create_http_request();
-}
-
-async fn short_way_to_create_http_request() {
-
-    let response_text = reqwest::get("http://127.0.0.1:5500/talha.html")
-        .await.expect("Couldent make request!")
-        .text()
-        .await.expect("Couldent read response text");
-
-    println!("response text will be {}", response_text);
-
-}
-
-
-/////////////////////////////method 3//////////////////////////////////////////////////
-
-// fn long_way_to_create_http_request() {
-
-//     // match reqwest::get("http://127.0.0.1:5500/talha.html"){
-
-//     //     Ok(mut response) => {
-
-//     //         //check if 200 ok
-//     //         if response.status() == reqwest :: StatusCode :: Ok {
-
-//     //             match response.text(){
-
-//     //                 Ok(text) => println!("Response Text: {}", text),
-//     //                 Err(_) => println!("Could not read response text! ")
-//     //             }
-//     //         } else {
-//     //             println!("Response was not 200 OK.");
-//     //         }
-//     //     }
-
-//     //     Err(_) => println!("Could not make the request!");
-//     // }
-
-// }
