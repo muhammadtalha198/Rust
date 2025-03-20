00aef90a188bd5e2cc56e77dc8bb2d75e7b2c10a
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-03-20 12:36:15 +0500

    Update main.rs

--- basicOfRust/modules/example2/main.rs
+++ basicOfRust/modules/example2/main.rs
@@ -1,1 +1,54 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
+
+mod back_of_house {
+
+    pub struct Breakfast {
+        pub toast: String,
+         seasonal_fruit: String,
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches") 
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house ::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+
+}
+
+
 
