a9f4077ee40221b24832f806281886c95f27a66a
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-03-21 11:20:05 +0500

    Create main.rs

---
+++ basicOfRust/LifeTimes/main.rs
@@ -0,0 +1,27 @@
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn shortest(x: &str, y: &str) -> String {
+    if x.len() > y.len() {
+        x.to_string()
+    } else {
+        y.to_string()
+    }
+}
+
+
+fn main() {
+    let string1 = String::from("My name is khan.");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is: {}", result);
+
+    let result1 = shortest(string1.as_str(), string2);
+    println!("The shortest string is: {}", result1);
+}
