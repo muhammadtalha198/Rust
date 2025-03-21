1c00e43c77f616fcdd1b254f56ca03b88921f995
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2025-03-21 10:34:59 +0500

    Create exmp3.rs

---
+++ basicOfRust/Generics/exmp3.rs
@@ -0,0 +1,44 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &T {
+        &self.y
+    } 
+ 
+}
+
+struct Point1<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point1<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point1<X2, Y2>) -> Point1<X1, Y2> {
+        Point1 {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+
+fn main() {
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
+    println!("p.y = {}", p.y());
+
+    let p1 = Point1 { x: 5, y: 10.4 };
+    let p2 = Point1 { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
